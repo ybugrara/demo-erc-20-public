48dffd2624d3ac9d2f163be586cd1f088a324a18
brianlai98 <brianclai@gmail.com>
2023-03-01 06:18:41 +0000

    update pkg

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,7 @@ import "@statespace/hardhat-testgen";
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
   testgen: {
-    targets: [],
-    depth: 1,
+    sequences: [["GLDToken.*"]],
   }
 };
 
