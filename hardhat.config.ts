f8e2d8459de7285f3825fa811032e08bb41f5414
brianlai98 <brianclai@gmail.com>
2023-03-01 08:10:33 +0000

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@statespace/hardhat-testgen";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.16",
-  testgen: {
-    sequences: [["GLDToken.*"]],
-  }
+  solidity: "0.8.16"
 };
 
 export default config;
